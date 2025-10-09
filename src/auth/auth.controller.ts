1bb0fe817476799dfa9ebfb761979cde3b3f7072
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-09 22:40:33 +0300

    feat(): add auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto, RegisterDto } from 'src/users/dto/user.dto';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+    @Post('register')
+    async register(@Body() user: RegisterDto) {
+        return this.authService.register(user);
+    }
+
+    @Post('login')
+    async login(@Body() user: LoginDto) {
+        return this.authService.login(user);
+    }
+}
