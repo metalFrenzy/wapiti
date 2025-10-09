a916b0e328a7aed4c8d24ef9c8a44d80da9fcc68
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-09 17:58:28 +0300

    feat(): add user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User])
   ],
   providers: [UsersService],
-  exports: [UsersService]
+  exports: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
