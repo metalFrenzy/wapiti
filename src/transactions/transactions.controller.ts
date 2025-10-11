4057819971aa8c97eb6ed69f8b91f8491314d675
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-11 23:57:40 +0300

    feat(): add controller

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateTransactionDto } from './dto/transaction.dto';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+    constructor(private readonly transactionService: TransactionsService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    createTransaction(@Body() createTransaction: CreateTransactionDto, @Request() req) {
+        return this.transactionService.create(createTransaction, req.user.id);
+    }
+}
+
+
