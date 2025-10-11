a90f586a15a2c24cf2c790b1971f9818cdf7e225
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-11 23:24:53 +0300

    feat(): update the service with the functions

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import { CreateTransactionDto } from './dto/transaction.dto';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+    constructor(@InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,) { }
+
+    async findAll(): Promise<Transaction[]> {
+        return this.transactionRepository.find();
+    }
+
+    async findById(id: string): Promise<Transaction | null> {
+        return this.transactionRepository.findOne({ where: { id } })
+    }
+
+    async create(transactionData: CreateTransactionDto): Promise<Transaction> {
+        const transaction = this.transactionRepository.create(transactionData)
+        return this.transactionRepository.save(transaction);
+    }
+
+    async delete(id: string, userId: string): Promise<void> {
+        await this.transactionRepository.delete({ id, userId });
+    }
+}
