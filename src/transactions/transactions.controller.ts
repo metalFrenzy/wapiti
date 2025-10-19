1aa5923c011f5a9f151c181d448312561181fbc2
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-19 22:06:51 +0300

    feat(): add new endpoint to the controller

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post, UseGuards, Request, Get, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Get, Param, Delete, Query } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { CreateTransactionDto } from './dto/transaction.dto';
+import { CreateTransactionDto, FilterTransactionDto } from './dto/transaction.dto';
 
 @Controller('transactions')
 export class TransactionsController {
@@ -15,8 +15,8 @@ export class TransactionsController {
 
     @UseGuards(JwtAuthGuard)
     @Get()
-    getAllTransactions(@Request() req) {
-        return this.transactionService.findAllByUser(req.user.id);
+    getAllTransactions(@Request() req, @Query() filters: FilterTransactionDto) {
+        return this.transactionService.findAllWithFilters(req.user.id, filters);
     }
 
     @UseGuards(JwtAuthGuard)
@@ -29,6 +29,12 @@ export class TransactionsController {
     delete(@Param('id') id: string, @Request() req) {
         return this.transactionService.delete(id, req.user.userId);
     }
+
+    @Get('statistics')
+    getStatistics(@Request() req, @Query('startDate') startDate?: string, @Query('endDate') endDate?: string) {
+        return this.transactionService.getStatistics(req.user.id, startDate, endDate)
+    }
+
 }
 
 
